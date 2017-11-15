aab1e6a032cc9e6baca669dbffca4cba7dbaa74b
mariusbd <dtrumarius16@gmail.com>
2017-11-15 10:48:08 +0000

    Modified server to serve public/index.html

--- server.js
+++ server.js
@@ -3,7 +3,18 @@ var sequelize = require("sequelize")
 var app = express()
 
 app.use(express.static('public'))
-app.use(express.json());       
-app.use(express.urlencoded({extended : true}));
+app.use('/public', express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+
+//connect to mysql database
+var sequelize = new sequelize('noteplus', 'root', '', {
+    dialect:'mysql',
+    host:'localhost'
+})
+
+sequelize.authenticate().then(function(){
+    console.log('Success')
+})
 
 app.listen(8080)
